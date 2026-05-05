cd0ce710901282d96808509e03baf8eb73fda869
BORGES <jessborgesb@gmail.com>
2026-05-05 16:41:48 +0200

    test: add BaseIntegrationTest with Testcontainers MongoDB and shard config

---
+++ src/test/java/cat/itacademy/s04/t02/n03/fruit_order_api/BaseIntegrationTest.java
@@ -0,0 +1,42 @@
+package cat.itacademy.s04.t02.n03.fruit_order_api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected MongoTemplate mongoTemplate;
+
+    @Container
+    protected static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @BeforeEach
+    void cleanDatabase() {
+        mongoTemplate.getCollectionNames().forEach(collection -> {
+            if (!collection.startsWith("system.")) {
+                mongoTemplate.dropCollection(collection);
+            }
+        });
+    }
+}
